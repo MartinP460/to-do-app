f8b615cd12fbbf041e2530e9fc57036b29860bb7
MartinP460 <martin.polley460gmail.com>
2021-12-23 15:48:32 +0100

    Modify files and add ESLint and TailwindCSS.

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ["plugin:react/recommended", "airbnb", "prettier"],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 13,
+    sourceType: "module",
+  },
+  plugins: ["react", "prettier"],
+  rules: {
+    "prettier/prettier": ["error"],
+    "func-names": ["off"],
+    "react/function-component-definition": ["off"],
+  },
+};
---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const App = () => (
+  <div className="flex justify-center">
+    <p>Hello world!</p>
+  </div>
+);
+
+export default App;
